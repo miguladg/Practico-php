ee316c72ac98464ae5f0c6b6833bef9b90791ada
migul <migul.a.d.g@gmail.com>
2023-06-10 13:13:40 -0500

    fiel-main

---
+++ main.php
@@ -0,0 +1,17 @@
+<?php
+
+$palabras = array("sol", "luna", "cielo");
+$palabrasDesordenadas = array();
+
+for ($i = 0; $i < 3; $i++) {
+    $palabrasDesordenadas[$i] = str_shuffle($palabras[$i]);
+}
+echo "
+<from>
+    <inpunt type='text' name='palabra0'>
+    <inpunt type='text' name='palabra1'>
+    <inpunt type='text' name='palabra2'>
+    <inpunt type='text' name='palabra3'>
+";
+
+?>
