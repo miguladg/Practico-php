da6879358d7c934dd59b9602de9a8f2294675c5b
migul <migul.a.d.g@gmail.com>
2023-06-13 08:15:41 -0500

    completo analisis

--- analisis.php
+++ analisis.php
@@ -1,3 +1,18 @@
 <?php
 print_r($_REQUEST);
+
+$palabras = array("sol", "luna", "cielo");
+
+if($_REQUEST["posicion0"] == $palabras[0]){
+    echo "La palabra ingresada es correcta";
+} else {
+    echo "La palabra es incorrecta: ".$palabras[0];
+} else {
+    echo "La palabra es incorrecta: ".$palabras[1];
+} else {
+    echo "La palabra es incorrecta: ".$palabras[2];
+} 
+
+if($_REQUEST["posicion1"] == $palabras[1])
+
 ?>
