7686799ec6b8489335a99381109dbdfabd493068
migul <migul.a.d.g@gmail.com>
2023-06-13 08:48:12 -0500

    correcion form

--- analisis.php
+++ analisis.php
@@ -3,16 +3,21 @@ print_r($_REQUEST);
 
 $palabras = array("sol", "luna", "cielo");
 
-if($_REQUEST["posicion0"] == $palabras[0]){
+if($_REQUEST["palabra0"] == $palabras[0]){
     echo "La palabra ingresada es correcta";
 } else {
     echo "La palabra es incorrecta: ".$palabras[0];
+}
+
+if($_REQUEST["palabra1"] == $palabras[1]){
+    echo "La palabra ingresada es correcta";
 } else {
     echo "La palabra es incorrecta: ".$palabras[1];
+}
+
+if($_REQUEST["palabra2"] == $palabras[2]){
+    echo "La palabra ingresada es correcta";
 } else {
     echo "La palabra es incorrecta: ".$palabras[2];
-} 
-
-if($_REQUEST["posicion1"] == $palabras[1])
-
+}
 ?>
--- main.php
+++ main.php
@@ -9,13 +9,12 @@ for ($i = 0; $i < 3; $i++) {
 
 print_r($palabrasDesordenadas);
 echo "
-<from action='analisis.php>
+<form action='analisis.php>
     <input type='text' name='palabra0'>
     <input type='text' name='palabra1'>
     <input type='text' name='palabra2'>
-    <input type='text' name='palabra3'>
     <button type='submit'> Enviar </button>
-</from>
+</form>
 ";
 
 ?>
